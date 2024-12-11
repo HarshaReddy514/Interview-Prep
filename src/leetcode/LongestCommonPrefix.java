2a7073a3a42d10e10fcae9d843fe59102c4d9120
HarshaReddy514 <devilharsha514@gmail.com>
2024-12-11 16:21:52 +0530

    First Leet code problem solved

--- src/leetcode/LongestCommonPrefix.java
+++ src/leetcode/LongestCommonPrefix.java
@@ -1,8 +1,5 @@
 package leetcode;
 
-import java.util.Arrays;
-import java.util.Comparator;
-
 public class LongestCommonPrefix {
     public static void main(String[] args) {
         String[] strs = {"aaa","aa","aaa"};
